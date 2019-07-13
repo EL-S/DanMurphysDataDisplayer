ac456c7db3d98a1433a525903f447a62f40d70b8
EL-S <31227083+EL-S@users.noreply.github.com>
2019-07-13 20:20:54 +1000

    Future Plans
    Plans to have cookies and storage for settings

--- search.php
+++ search.php
@@ -1,5 +1,6 @@
 <?php
 
 // take a search query and settings and run a query against the database for the js to handle and display, return json or csv maybe
+// have multiple checkboxes that are remembered between sessions
 
 ?>

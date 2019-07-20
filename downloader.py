44913b0598a08a1ad474a0582f5fba7f0645b4d3
EL-S <31227083+EL-S@users.noreply.github.com>
2019-07-20 22:55:22 +1000

    Future Plans
    Future Plans

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
 # run as a cron that downloads all new products and updated products on danmurphy's site
 # upload all sanitised data to the SQL database
 # abide by robots.txt and use the product sitemap
+# actively check robots.txt

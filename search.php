e8a7765c0019d55da7bac3200afded5c93165db9
EL-S <31227083+EL-S@users.noreply.github.com>
2019-07-27 23:54:10 +1000

    Future Plans
    Plans for the future

--- search.php
+++ search.php
@@ -3,5 +3,6 @@
 // take a search query and settings and run a query against the database for the js to handle and display, return json or csv maybe
 // have multiple checkboxes that are remembered between sessions
 // use the session data to recommend products
+// allow ease of api use
 
 ?>
